d93f2ee9da414a14c0d49902767f3f880e47431d
David Atkinon <david.atkinson@netzero.com>
2021-11-30 10:00:11 -0600

    updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Four_Motor_Minibot_Meccanum_Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Four_Motor_Minibot_Meccanum_Drivetrain.java
@@ -49,17 +49,17 @@ public class Four_Motor_Minibot_Meccanum_Drivetrain {
             leftRear = hwMap.get(DcMotor.class, "Left_rear");
             rightRear = hwMap.get(DcMotor.class, "Right_rear");
 
-            // For Mecanum #4 and #6
+            // For Mecanum #4, #5  and #6
             leftFront.setDirection(DcMotor.Direction.REVERSE);
             rightFront.setDirection(DcMotor.Direction.FORWARD);
             rightRear.setDirection(DcMotor.Direction.FORWARD);
             leftRear.setDirection(DcMotor.Direction.REVERSE);
 
-            // For Mecanum #3 comment out if using 4 or 6
-            leftFront.setDirection(DcMotor.Direction.FORWARD);
-            rightFront.setDirection(DcMotor.Direction.REVERSE);
-            rightRear.setDirection(DcMotor.Direction.REVERSE);
-            leftRear.setDirection(DcMotor.Direction.FORWARD);
+            // For Mecanum #3
+            //leftFront.setDirection(DcMotor.Direction.FORWARD);
+            //rightFront.setDirection(DcMotor.Direction.REVERSE);
+            //rightRear.setDirection(DcMotor.Direction.REVERSE);
+            //leftRear.setDirection(DcMotor.Direction.FORWARD);
 
 
             leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
